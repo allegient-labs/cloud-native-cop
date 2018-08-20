41747570fd50e8adef58f177012fa3d838df629f
Musema Hassen <musema.hassen@gmail.com>
2018-08-20 15:55:21 -0400

    Initial commit

---
+++ src/app/models/Blog.ts
@@ -0,0 +1,13 @@
+export interface Blog {
+    blogId?: string;
+    blogTopic?: string;
+    blogDescription?: string;
+    blogCategory?: string;
+    startTime?: string;
+    endTime?: string;
+    blogHost?: string;
+    note?: string;
+    places?: object;
+    presenters?: any;
+    approvers?: any;
+}
