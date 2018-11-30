d05ba4dda2a02dd00ba0ed09022c1b016271d016
venupendiyala <vreddy.venu@gmail.com>
2018-11-29 21:13:16 -0500

    intial commit with readMe

--- appsync/frontend/src/app/graphql/appSyncConfig.ts
+++
@@ -1,8 +0,0 @@
-
-export const appSyncConfig = {
-  'aws_appsync_graphqlEndpoint': '<GRAPHQL_URL_HERE>',
-  'aws_appsync_region': 'us-east-1',
-  'aws_appsync_authenticationType': 'API_KEY',
-  'aws_appsync_apiKey': '<API_KEY_HERE>'
-};
-
--- appsync/frontend/src/app/models/Blog.ts
+++
@@ -1,8 +0,0 @@
-export interface Blog {
-    id?: string;
-    blogCategory?: string;
-    blogDescription?: string;
-    bloggerEmail?: string;
-    likes?: number;
-    disLikes?: number;
-}
